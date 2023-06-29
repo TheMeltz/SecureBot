42f5c4c94627057200e3b1d9ffb65d8714f5356f
MeltzDev <guto7654Dev@gmail.com>
2023-06-29 13:07:56 -0300

    Roblox Userinfo API Finish

--- src/commands/utility/ban.ts
+++ src/commands/utility/ban.ts
@@ -1,23 +1,60 @@
-import { CacheType, CommandInteractionOptionResolver, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CacheType, CommandInteractionOptionResolver, EmbedBuilder, Interaction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import fetchUserFromIdentifier from "../../api/Userinfo";
 
-async function execute(interaction: { options: CommandInteractionOptionResolver<CacheType>; reply: (arg0: string) => any; }) {
-    const options: CommandInteractionOptionResolver = interaction.options;
+//@ts-ignore
+async function execute(interaction) {
+    //@ts-ignore
+    const options: CommandInteractionOptionResolver<CacheType> = interaction.options;
     const identifier = options.getString('identifier')
-    const reason = interaction.options.getString('reason', false) ?? 'No reason provided';
+    const reason = options.getString('reason', false) ?? 'No reason provided';
+    const duration = options.getString('duration') ?? 'Permanent';
     const user = await fetchUserFromIdentifier(identifier!);
 
     if (user?.status == 400) {
        return await interaction.reply(`User with identifier: ${identifier} was not found!`);
     }
 
-    return await interaction.reply(`Banning ${user.data.Username} for reason: ${reason}\n${user.data.Image}`);
+    const confirm = new ButtonBuilder()
+        .setCustomId('confirm')
+        .setEmoji('🔨')
+        .setLabel('Confirm ban')
+        .setStyle(ButtonStyle.Danger)
+
+    const cancel = new ButtonBuilder()
+        .setCustomId('cancel')
+        .setLabel('Cancel')
+        .setEmoji('❌')
+        .setStyle(ButtonStyle.Secondary)
+
+    const row = new ActionRowBuilder()
+        .addComponents(confirm, cancel)
+
+    const embed = new EmbedBuilder()
+        .setTitle(`Are you sure you want to ban ${user.data.Username}(Aka ${user.data.DisplayName})?`)
+        .setColor('Red')
+        .setDescription(`**User description:**\n${user.data.Description}`)
+        .addFields(
+            { name: 'UserId', value: user.data.Id.toString(), inline: true },
+            { name: 'Ban Duration', value: duration, inline: true },
+            { name: 'Reason', value: reason, inline: true }
+        )
+        .setThumbnail(user.data.Image)
+        .setTimestamp()
+
+    //@ts-ignore
+    return await interaction.reply({
+        content: `Please, confirm that you want to ban ${user.data.Username}`,
+        embeds: [embed],
+        components: [row],
+        ephemeral: true
+    });
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
         .setDescription('Bans a roblox player using an unique identifier!')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .addStringOption(option => option.setName('identifier')
             .setDescription('The roblox Username or UserId of the player you wants to ban.')
             .setRequired(true)
